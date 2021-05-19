31e83ecc1eb68cb621344d163b534505fc6d6e89
girishghoda <girish.ghoda@ajmerainfotech.com>
2021-05-19 11:36:55 +0530

    Update Function1.cs

--- AzureFunctionPiepline/Function1.cs
+++ AzureFunctionPiepline/Function1.cs
@@ -14,7 +14,7 @@ namespace AzureFunctionPiepline
     {
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -26,7 +26,7 @@ namespace AzureFunctionPiepline
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                ? "Hanji!! This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
